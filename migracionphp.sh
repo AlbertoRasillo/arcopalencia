90c7435109cb27b3059470b69604d03cde22db57
AlbertoRasillo <bluesnatural@gmail.com>
2018-01-19 10:15:18 +0000

    added new patterns and exclude self migration file to replace patterns

--- migracionphp.sh
+++ migracionphp.sh
@@ -8,17 +8,17 @@ find . -type f -exec chmod 644 -- {} +
 
 #Con estos comandos de terminal cambiamos las funciones de php para que sean soportadas en PHP7
 
-grep -rl 'mysql_real_escape_string(' . | xargs sed -i 's/mysql_real_escape_string(/mysqli_real_escape_string($con,/g'
+grep -rl --exclude=migracionphp.sh 'mysql_real_escape_string(' . | xargs sed -i 's/mysql_real_escape_string(/mysqli_real_escape_string($con,/g'
 
-grep -rl 'mysql_fetch_array($fila)' . | xargs sed -i 's/mysql_fetch_array($fila)/mysqli_fetch_array($fila,MYSQLI_ASSOC)/g'
+grep -rl --exclude=migracionphp.sh 'mysql_fetch_array($fila)' . | xargs sed -i 's/mysql_fetch_array($fila)/mysqli_fetch_array($fila,MYSQLI_ASSOC)/g'
 
-grep -rl 'mysql_fetch_array($buscar)' . | xargs sed -i 's/mysql_fetch_array($buscar)/mysqli_fetch_array($buscar,MYSQLI_ASSOC)/g'
+grep -rl --exclude=migracionphp.sh 'mysql_fetch_array($buscar)' . | xargs sed -i 's/mysql_fetch_array($buscar)/mysqli_fetch_array($buscar,MYSQLI_ASSOC)/g'
 
-grep -rl 'mysql_query(' . | xargs sed -i 's/mysql_query(/mysqli_query($con,/g'
+grep -rl --exclude=migracionphp.sh 'mysql_query(' . | xargs sed -i 's/mysql_query(/mysqli_query($con,/g'
 
-grep -rl 'mysql_error' . | xargs sed -i 's/mysql_error/mysqli_error/g'
+grep -rl --exclude=migracionphp.sh 'mysql_error' . | xargs sed -i 's/mysql_error/mysqli_error/g'
 
-grep -rl 'mysql_close(),' . | xargs sed -i 's/mysqli_close(),/mysqli_close($con),/g'
+grep -rl --exclude=migracionphp.sh 'mysql_close(),' . | xargs sed -i 's/mysqli_close(),/mysqli_close($con),/g'
 
-grep -rl 'mysql_fetch_assoc' . | xargs sed -i 's/mysql_fetch_assoc/mysqli_fetch_assoc/g'
+grep -rl --exclude=migracionphp.sh 'mysql_fetch_assoc' . | xargs sed -i 's/mysql_fetch_assoc/mysqli_fetch_assoc/g'
 
