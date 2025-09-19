41e0ce790683a0b3934abc44ed7b25aa8de9c8a0
AlbertoRasillo <bluesnatural@gmail.com>
2025-09-19 08:43:39 +0000

    Change: date pedidos

--- comprobar_fecha.php
+++ comprobar_fecha.php
@@ -7,24 +7,20 @@
     $horariopedido=1;
     $fecha_actual = strftime("%w");
     $hora_actual = date("His");
-    $horafinal = 221000;
-    $horafinal_lun = 120000;
-    if ($fecha_actual=="4" or $fecha_actual=="5"
-        or $fecha_actual=="6" or $fecha_actual=="0" or $fecha_actual=="1") {
-            /* Quitar comentarios si queremos que el horario pedido termine el domingo a las 22.00
-            if ($fecha_actual=="0" and $hora_actual<=$horafinal) {
-            }
-            if ($fecha_actual=="0" and $hora_actual>=$horafinal) {
-                $horariopedido=0;
-            }
-            */
-            /* Horario pedido termina el lunes a las 12.00 */
-            if ($fecha_actual=="1" and $hora_actual<=$horafinal_lun) {
-            }
-            if ($fecha_actual=="1" and $hora_actual>=$horafinal_lun) {
-                $horariopedido=0;
-            }
-    }else{
-            $horariopedido=0;
+    $horafinal_vie = 120000; // Viernes hasta las 12:00 PM
+
+    if ($fecha_actual == "2" || $fecha_actual == "3" || $fecha_actual == "4") {
+        // Martes, miércoles, jueves -> pedidos habilitados todo el día
+        $horariopedido = 1;
+    } elseif ($fecha_actual == "5") {
+        // Viernes -> hasta las 12:00 PM
+        if ($hora_actual <= $horafinal_vie) {
+            $horariopedido = 1;
+        } else {
+            $horariopedido = 0;
         }
+    } else {
+        // Sábado, domingo, lunes -> pedidos deshabilitados
+        $horariopedido = 0;
+    }
 ?>
